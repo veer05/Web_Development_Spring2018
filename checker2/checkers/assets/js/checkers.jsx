71c077813bbf39ad0f7468ab3331eb29b56d8647
veer05 <veeresh05@gmail.com>
2018-03-23 01:23:34 -0400

    codecleaned

--- checker2/checkers/assets/js/checkers.jsx
+++ checker2/checkers/assets/js/checkers.jsx
@@ -8,11 +8,9 @@ export default function game_init(root,channel) {
   ReactDOM.render(<Checkers channel={channel} />, root);
 }
  
-
 class Checkers extends React.Component {
  
   constructor(props) {
-    
     super(props);
     this.channel = props.channel;
     this.state = {
@@ -24,229 +22,25 @@ class Checkers extends React.Component {
    this.channel.join()
         .receive("ok", this.renderView.bind(this))
         .receive("error",resp => {console.log("unable to join",resp)});
-
-
  }
-
-
 
  renderView(view){
 
-    console.log("inside render view");
-    console.log(view.game);
     this.setState(view.game);
-    console.log("after set state");
-    console.log(this.state);
-  
   }
 
-  // TO CHANGE
   getSquares()
   {
     let allSquares = [];
-
     for(let i=0;i<64;i++)
     {
-      let oneSquare = <Square id={i} key={i} pawns={this.state.pawns} prevclick={this.state.previously_clicked} player={this.state.previous_player} pawnClicked={this.pawnClicked.bind(this)} possibleNextMove={this.getNextMove.bind(this)} 
+      let oneSquare = <Square id={i} key={i} pawns={this.state.pawns} prevclick={this.state.previously_clicked} player={this.state.previous_player} pawnClicked={this.pawnClicked.bind(this)}
       dict = {this.state.validSquares}
       movepawn={this.movepawn.bind(this)}/>;
       allSquares.push(oneSquare);
     }
-
     return allSquares;
   }
-
-  
-  getNextMove(pawn,color){
-	  	console.log('at getNextMove')
-	  	console.log(this.state.validSquares)
-    //let temp = this.state.validSquares
-
-/*    if(Object.keys(temp).length == 0){
-      this.channel.push("getNextPos", {id: pawn.id, color: pawn.player_color})
-                .receive("ok", this.renderView.bind(this));
-    }*/
-    //console.log('at getNextMove')
-    //return this.state.validSquares
-    //console.log(pawn);
-    /*let makepawns={};
-    let dictmove1 = {};
-    let dictmove2 = {}; 
-    if(pawn.king == true){
-      dictmove1 = this.getNextRedMove(pawn);
-      dictmove2 = this.getNextBlackMove(pawn);
-      Object.assign(makepawns,dictmove1,dictmove2)
-    }
-    else if (pawn.player_color == "red"){
-      makepawns = this.getNextRedMove(pawn);
-    }
-    else {
-      makepawns = this.getNextBlackMove(pawn);
-    }
-    //console.log("GetNextMove")
-    //console.log(makepawns)
-    return makepawns;*/
-  }
-
-
-  getNextRedMove(pawn){
-    let dictmove = {};
-    let makepawns=[];  
-    let pawnlist = this.state.pawns;
-    makepawns[0] = pawn.position + 7;    
-    makepawns[1] = pawn.position + 9;
-
-    //console.log("At getNextMove RED");
-    if(pawn.position % 8 == 0){
-        makepawns[0] = 100;
-    }
-    if((pawn.position+1) % 8 == 0){
-        makepawns[1] = 100;
-    }
-    if(pawn.player_color == "black"){
-        for(let i=0;i<12;i++)
-      {   
-        if(pawnlist.red[i].position == makepawns[0]){
-            makepawns[0] = pawn.position + 14;    
-              for(let i=0;i<12;i++)
-                {
-                 if(pawnlist.red[i].position == makepawns[0] || pawnlist.black[i].position == makepawns[0]|| ((makepawns[0] + 1) % 8 == 0)) {
-                    makepawns[0] = 100        
-                 } 
-                }
-          }
-          else if(pawnlist.black[i].position == makepawns[0]){
-            makepawns[0] = 100
-          }
-          if (pawnlist.red[i].position == makepawns[1]){
-              makepawns[1] = pawn.position + 18;
-                for(let i=0;i<12;i++)
-                {
-                 if(pawnlist.red[i].position == makepawns[1] || pawnlist.red[i].position == makepawns[1] ||(makepawns[1]) % 8 == 0){
-                    makepawns[1] = 100        
-                 } 
-                }
-          }   
-          else if(pawnlist.black[i].position == makepawns[1]){
-             makepawns[1] = 100
-          }
-      }
-    }
-    else{
-      for(let i=0;i<12;i++)
-      {   
-        if(pawnlist.black[i].position == makepawns[0]){     
-            makepawns[0] = pawn.position + 14;
-              for(let i=0;i<12;i++)
-                {
-                 if(pawnlist.black[i].position == makepawns[0] || pawnlist.red[i].position == makepawns[0]|| ((makepawns[0] + 1) % 8 == 0)) {
-                    makepawns[0] = 100        
-                 } 
-                }
-          }
-          else if(pawnlist.red[i].position == makepawns[0]){
-            makepawns[0] = 100
-          }
-          if (pawnlist.black[i].position == makepawns[1]){
-              makepawns[1] = pawn.position + 18;
-                for(let i=0;i<12;i++)
-                {
-                 if(pawnlist.black[i].position == makepawns[1] || pawnlist.black[i].position == makepawns[1] ||(makepawns[1]) % 8 == 0){
-                    makepawns[1] = 100        
-                 } 
-                }
-          }   
-          else if(pawnlist.red[i].position == makepawns[1]){
-             makepawns[1] = 100
-          }
-      }
-    }
-
-
-    dictmove[makepawns[0]] = true;
-    dictmove[makepawns[1]] = true;
-    return dictmove;
-  }
-
-  getNextBlackMove(pawn){
-    let dictmove={};
-    let makepawns=[];  
-    let pawnlist = this.state.pawns;
-    makepawns[0] = pawn.position - 9;    
-    makepawns[1] = pawn.position - 7;
-    console.log("At getNextMove Black");
-    
-    
-    if(pawn.position % 8 == 0){
-        makepawns[0] = 100;
-    }
-    if((pawn.position+1) % 8 == 0){
-        makepawns[1] = 100;
-    }
-    if (pawn.player_color == "red")
-    {
-      for(let i=0;i<12;i++)
-      {   
-        if(pawnlist.black[i].position == makepawns[0]){     
-            makepawns[0] = pawn.position - 18;
-              for(let i=0;i<12;i++)
-                {
-                 if(pawnlist.red[i].position == makepawns[0] || pawnlist.black[i].position == makepawns[0]|| ((makepawns[0] + 1) % 8 == 0)) {
-                    makepawns[0] = 100        
-                 } 
-                }
-          }
-          else if(pawnlist.red[i].position == makepawns[0]){
-            makepawns[0] = 100
-          }
-          if (pawnlist.black[i].position == makepawns[1]){
-              makepawns[1] = pawn.position - 14;
-                for(let i=0;i<12;i++)
-                {
-                 if(pawnlist.red[i].position == makepawns[1] || pawnlist.black[i].position == makepawns[1] ||(makepawns[1]) % 8 == 0){
-                    makepawns[1] = 100        
-                 } 
-                }
-          }   
-          else if(pawnlist.red[i].position == makepawns[1]){
-             makepawns[1] = 100
-          }
-      }
-    }
-    else{
-    for(let i=0;i<12;i++)
-      {   
-        if(pawnlist.red[i].position == makepawns[0]){
-            makepawns[0] = pawn.position - 18;    
-              for(let i=0;i<12;i++)
-                {
-                 if(pawnlist.black[i].position == makepawns[0] || pawnlist.red[i].position == makepawns[0]|| ((makepawns[0] + 1) % 8 == 0)) {
-                    makepawns[0] = 100        
-                 } 
-                }
-          }
-          else if(pawnlist.black[i].position == makepawns[0]){
-            makepawns[0] = 100
-          }
-          if (pawnlist.red[i].position == makepawns[1]){
-              makepawns[1] = pawn.position - 14;            
-                for(let i=0;i<12;i++)
-                {
-                 if(pawnlist.black[i].position == makepawns[1] || pawnlist.red[i].position == makepawns[1] ||(makepawns[1]) % 8 == 0){
-                    makepawns[1] = 100        
-                 } 
-                }
-          }   
-          else if(pawnlist.black[i].position == makepawns[1]){
-             makepawns[1] = 100
-          }
-      }
-    }
-      dictmove[makepawns[0]] = true;
-      dictmove[makepawns[1]] = true;
-      return dictmove;
-  }
-
 
   pawnToRemove(remove_pawn, pos){
     let pawnlist = this.state.pawns;
@@ -259,89 +53,45 @@ class Checkers extends React.Component {
   }
 
   movepawn(id,pawn_id,color){
-  	console.log(id)
-  	console.log(pawn_id)
-  	console.log(color)
+
     this.channel.push("movepawn", {id: id, pawn_id: pawn_id, color: color})
                 .receive("ok", this.renderView.bind(this));
-  
   }
 
-  pawnClicked(id,pawn_id,color)
+  pawnClicked(id,pawn_id,color,player)
   {
-
-    console.log("At Square Position")
-    console.log(id)
-    console.log("pawn clicked");
-    console.log(pawn_id)
-    console.log("pawn color");
-    console.log(color)
-    
     var valid_pos1;
     var valid_pos2;
+    this.setState({previously_clicked:pawn_id})
+    this.setState({previous_player:color})
+    console.log('Setting the Dict')
+    let temp = this.state.validSquares
 
-    // Get the Pawn Object 
-    //var selected_pawn;
-    //var valid_move1;
-    //val idmovear valid_move2;
-
-    console.log("this is prev click");
-    console.log(this.state.previously_clicked);
-    //if (this.state.previously_clicked !== 100){
-    //      this.setState({previously_clicked:100})
-    //      this.setState({previous_player:'none'})
-    //      this.setState({validSquares:{}})  
-    //}
-    //else{
-          //CHange made for setstate
-          //pawn_id = this.state.pawns[color][pawn_id]
-          //this.getNextMove.bind(this,pawn_id,color);
-          this.setState({previously_clicked:pawn_id})
-          this.setState({previous_player:color})
-          //this.getNextMove.bind(pawn_id,color);
-          console.log('Setting the Dict')
-    	  let temp = this.state.validSquares
-
-    	  //if(Object.keys(temp).length == 0){
-    	  //console.log('InsideSetting the Dict')
-      	  this.channel.push("getNextPos", {id: pawn_id, color: color})
-    	              .receive("ok", this.renderView.bind(this));
-    //}
-
-    //}
+    if(this.state.nextChance === color){
+      
+      this.channel.push("getNextPos", {id: pawn_id, color: color})
+               .receive("ok", this.renderView.bind(this));
+    }
     
-    /*if(color == 'red')
-    {
-      selected_pawn = this.state.pawns.red[pawn_id];
-      console.log(this.state.pawns.red[pawn_id].position+7)
-      console.log(this.state.pawns.red[pawn_id].position+9)
-    }
-    else{
-      selected_pawn = this.state.pawns.black[pawn_id]
-      console.log(this.state.pawns.black[pawn_id].position-7)
-      console.log(this.state.pawns.black[pawn_id].position-9)
-    }
-*/
-    //console.log(selected_pawn);
  }
 
   render()
   {
-
-      if (this.state.pawns.length != 0){
-        return(
-            <div id="gameboard">
+    if (this.state.pawns.length != 0){
+      return(
+         <div id="gameboard">
             {this.getSquares()}
-            </div>
+          </div>
         );
       }
-      else{
-          return null;
-      }
+    else{
+      return null;
+    }
   }
 }
 
 function Square(props) {
+
   const {id, pawns}= props;
   let found = 'none'
   let p_id = 100
@@ -349,31 +99,28 @@ function Square(props) {
   var highlight_Square = false;
   var normal_Square = true;
   if((parseInt(id / 8))%2==0)
-  {
-    
+  {   
     if(id % 2 == 0)
     {
-      color = "BurlyWood";
+      color = "BurlyWood";//1#9f0707
     }
     else
     {
-      color = "#663300";
+      color = "#663300";//2#090909
     }
   } 
   else
   {
     if(id % 2 == 0)
     {
-      color = "#663300";
+      color = "#663300";//2
     }
     else
     {
-      color = "BurlyWood";
+      color = "BurlyWood";//1
     }
   }
-  
   var clickable = false;
-
   for(let i=0;i<12;i++)
   {
         if(pawns.red[i].position === id) {
@@ -383,48 +130,13 @@ function Square(props) {
         else if (pawns.black[i].position === id){
             found = 'black'
             p_id = pawns.black[i].id
-          }
+        }
   } 
-
   if(props.dict[id] !== undefined){
           clickable = true;
           highlight_Square = true;
           normal_Square = false;
   }
-  /*let validmove = {};
-  if (props.player == 'red'){
-      let pawn = pawns.red[props.prevclick];
-      //	console.log(pawn);
-      //if (pawns.red[props.prevclick].position == id){
-      //validmove = props.possibleNextMove(pawn,color);
-      //props.pawnClicked(21, 10, "red")
-      //}
-      
-      if(props.dict[id] !== undefined){
-          //console.log(id)
-          clickable = true;
-          highlight_Square = true;
-          normal_Square = false;
-  }
-
-  }
-  else if(props.player == 'black'){
-      let pawn = pawns.black[props.prevclick];
-      //let validmove = props.possibleNextMove(pawn,color); 
-      //let position = pawns.black[props.prevclick].position; 
-      //let validmove1 = position-7;
-      //let validmove2 = position-9;
-      if(props.dict[id] !== undefined){
-            clickable = true;
-            highlight_Square = true;
-            normal_Square = false;
-      }
-  }*/
-
-  //  console.log(found)
-  //  console.log(p_id)
- //Attribution: 
-  
   var className = classnames(
                'empty':true,
               {'red pawn king': (found === 'red') && (pawns.red[p_id].king) === true},
@@ -436,12 +148,10 @@ function Square(props) {
           {'square': normal_Square},
           {'square_highlight': highlight_Square}  
   );
-
   return (
-            <div className= {hightlightclass} style={{backgroundColor: color}} 
+          <div className= {hightlightclass} style={{backgroundColor: color}} 
             onClick={clickable ? () => props.movepawn(id, props.prevclick, props.player) : null}>
-              <div className ={className} onClick={() => props.pawnClicked(id, p_id, found)}/>
-            </div>
-
+          <div className ={className} onClick={() => props.pawnClicked(id, p_id, found, props.player)}/>
+          </div>
   );
 }
--- checker2/checkers/lib/checkers/game.ex
+++ checker2/checkers/lib/checkers/game.ex
@@ -1,21 +1,20 @@
 defmodule Checkers.Game do
 
-
+  # create new instance of the game
 	def new do
 	IO.inspect("oin new")
-    %{
-        
+    %{  
        	pawns: %{"red" => createPlayerPawns("red"), 
        	         "black" => createPlayerPawns("black")},
 
        	previously_clicked: 100,
        	previous_player: "none",
         validSquares: %{},
-
+        nextChance: "red",
     }
 	end
 
-
+  #set the current game state
 	def client_view(game) do
 	IO.inspect("in client_view")   
    %{
@@ -23,13 +22,14 @@ defmodule Checkers.Game do
         pawns: game[:pawns],
         previously_clicked: game.previously_clicked,
        	previous_player: game.previous_player,
-        validSquares: game[:validSquares]
+        validSquares: game[:validSquares],
+        nextChance: game.nextChance,
     } 
   	end
 
+  #create red and black pawns and respective positions  
 	def createPlayerPawns(color) do
 	
-
 		make_pawns=[]
     if(color == "red") do
     	new_array = [{0,1},{1,3},{2,5},{3,7},{4,8},{5,10},{6,12},{7,14},{8,17},{9,19},{10,21},{11,23}];
@@ -42,17 +42,27 @@ defmodule Checkers.Game do
   		make_pawns = make_pawns ++ %{id: k, player_color: color, position: v, defeated: false,king: false} 
 
   		 end)
-
-		
 	end
 
+  def selectPawnToRemove(game,selectedPawn,remove_pawn,id) do   
+    cond do
+      id == selectedPawn.position + 14 ->
+              rm_pawn = pawnToRemove(game,remove_pawn, id - 7)
+      id == selectedPawn.position + 18 ->
+              rm_pawn = pawnToRemove(game,remove_pawn, id - 9)
+      id == selectedPawn.position - 14 ->
+              rm_pawn = pawnToRemove(game,remove_pawn, id + 7)
+      id == selectedPawn.position - 18 ->
+              rm_pawn = pawnToRemove(game,remove_pawn, id + 9)
+      true -> 
+              rm_pawn = []
+    end
+  end
 
+  #move pawn based on the valid positions obtained
   def movepawn(game,id,pawn_id,color) do
-    IO.inspect("at elixir movepawn")
-    IO.inspect(color)
     pawns = game[:pawns]
     selectedPawns = pawns[color]
-    IO.inspect(selectedPawns)
     selectedPawn = Enum.at(selectedPawns,pawn_id)
     if(color == "red") do
       remove_pawn = "black"
@@ -62,84 +72,55 @@ defmodule Checkers.Game do
       tempId = 8
     end
     removePawns = pawns[remove_pawn]
-    IO.inspect(removePawns)
-    cond do
 
-      id == selectedPawn.position + 14 ->
-              rm_pawn = pawnToRemove(game,remove_pawn, id - 7)
-
-      id == selectedPawn.position + 18 ->
-              rm_pawn = pawnToRemove(game,remove_pawn, id - 9)
-
-      id == selectedPawn.position - 14 ->
-              rm_pawn = pawnToRemove(game,remove_pawn, id + 7)
-
-      id == selectedPawn.position - 18 ->
-              rm_pawn = pawnToRemove(game,remove_pawn, id + 9)
-
-      true -> 
-              rm_pawn = []
-    end
-    IO.inspect("This is rm_pawn")
-    IO.inspect(rm_pawn)
+    rm_pawn = selectPawnToRemove(game,selectedPawn,remove_pawn,id)
+    #move the selected pawn
     newSelectedPawns = []
     newSelectedPawns = Enum.map(selectedPawns, fn(x) -> 
 
       if(x.id == pawn_id ) do
-
         if((color =="red" and id > tempId) or (color == "black" and id<tempId)) do
-
           newSelectedPawns = newSelectedPawns ++ %{defeated: x.defeated, id: x.id, player_color: x.player_color, king: true, position: id}
-
         else
-
           newSelectedPawns = newSelectedPawns ++ %{defeated: x.defeated, id: x.id, player_color: x.player_color, king: x.king, position: id}
-
         end
-
       else
-
         newSelectedPawns = newSelectedPawns ++ x
-
     end end)
 
+    #remove the pawns if jump happens
     newRemovePawns = []
     if((length rm_pawn) != 0) do
 
       pawn = Enum.at(rm_pawn,0)
-      IO.inspect("in rm0")
       newRemovePawns = Enum.map(removePawns, fn(x) -> 
-
         if(x.id == pawn.id) do
-
           newRemovePawns = newRemovePawns ++ %{defeated: x.defeated, id: x.id, player_color: x.player_color, king: x.king, position: -100}
-
         else
-
           newRemovePawns = newRemovePawns ++ x
-
       end end)
     else
-
       newRemovePawns = newRemovePawns ++ removePawns
-
     end
 
     if(color == "red") do
       newPawns = %{"red" => newSelectedPawns, "black" => newRemovePawns}
-
     else
-
       newPawns = %{"black" => newSelectedPawns, "red" => newRemovePawns}
     end
-    
+    #set the game states with the updated values
     game = Map.put(game, :pawns, newPawns)
     game = %{game | previously_clicked: 100 }
     game = %{game | previous_player: 'none' }
     game = %{game | validSquares: %{}}
-  
+    if(color == "red") do
+      game = %{game | nextChance: "black"}
+    else
+      game = %{game | nextChance: "red"}
+    end
   end
 
+  #remove the pawn selected for removal
   def pawnToRemove(game,remove_pawn, pos) do
 
     pawns = game[:pawns]
@@ -148,170 +129,131 @@ defmodule Checkers.Game do
     Enum.filter(removePawns, fn(x) -> 
       x.position == pos 
     end)
-
   end
 
-
+  #get next valid positions
   def getNextPos(game,id,color) do
 
     pawns = game[:pawns]
     pawnType = pawns[color]
     pawn = Enum.at(pawnType,id)
-    IO.inspect("in getnextpos")
     makePawns=%{};
     dictmove1 = %{};
     dictmove2 = %{}; 
+    #condition to check for king
     cond do
       pawn.king == true ->
-
                     dictmove1 = getNextRedMove(game,pawn)
                     dictmove2 = getNextBlackMove(game,pawn)
                     makePawns = Map.merge(dictmove1,dictmove2)
-
 
       pawn.player_color == "red" ->
                     makePawns = getNextRedMove(game,pawn)
 
       true ->
                     makePawns = getNextBlackMove(game,pawn)
-
     end
-
+    #set the current value for valid squares 
     game = Map.put(game, :validSquares, makePawns)
     game = %{game | previously_clicked: id }
     game = %{game | previous_player: color }
-
   end
 
+  #get next position for black pawns when opponent is red
   def getBlackPlayer(newRedMap,newBlackMap,pos0,pos1,pawn) do
 
       validPos = %{}
       cond do
-
         newRedMap[pos0]!=nil ->
-
                pos0 = pawn.position + 14
                if((newRedMap[pos0]!=nil) or (newBlackMap[pos0]!=nil) or (rem(pos0 + 1, 8) == 0)) do
                 pos0 = 100
                end 
-
         newBlackMap[pos0]!=nil ->
-
                 pos0 = 100
-
+        true ->
+                pos0
       end
-
       cond do
-
         newRedMap[pos1]!=nil ->
-
                pos1 = pawn.position + 18
                if((newRedMap[pos1]!=nil) or (newBlackMap[pos1]!=nil) or (rem(pos1, 8) == 0)) do
                 pos1 = 100
                end 
-
         newBlackMap[pos1]!=nil ->
 
                 pos1 = 100
-
+        true -> 
+                pos1
       end  
-
       validPos = %{pos0 => true, pos1 => true}
-
   end
 
-
+  #get red position when opponent is black
   def getRedPlayer(newRedMap,newBlackMap,pos0,pos1,pawn) do
 
       validPos = %{}
       cond do
-
         newBlackMap[pos0]!=nil ->
-
                pos0 = pawn.position + 14
                if((newRedMap[pos0]!=nil) or (newBlackMap[pos0]!=nil) or (rem(pos0 + 1, 8) == 0)) do
                 pos0 = 100
                end 
-
         newRedMap[pos0]!=nil ->
-
                 pos0 = 100
         true ->
                 pos0
-
       end
-
       cond do
-
         newBlackMap[pos1]!=nil ->
-
                pos1 = pawn.position + 18
                if((newRedMap[pos1]!=nil) or (newBlackMap[pos1]!=nil) or (rem(pos1, 8) == 0)) do
                 pos1 = 100
                end 
-
         newRedMap[pos1]!=nil ->
 
                 pos1 = 100
-
         true -> 
                 pos1
-
       end  
-
       validPos = %{pos0 => true, pos1 => true}
   end
 
-
-
+  #get next position for the red
   def getNextRedMove(game,pawn) do
-    IO.inspect("in getNextRedMove")
+
     dictmove = %{}
     makepawns=[]  
     pawns = game[:pawns]
     pos0 = pawn.position + 7
-    pos1 = pawn.position + 9
-    
+    pos1 = pawn.position + 9  
     redPawns = pawns["red"]
     blackPawns = pawns["black"]
-
     newRedMap = %{}
     newBlackMap = %{}
 
     newRedMap = Enum.reduce(redPawns, %{},fn(x,acc) ->  
-
       Map.put(acc, x.position, x)
-
     end)
 
     newBlackMap = Enum.reduce(blackPawns, %{},fn(x,acc) ->  
-
       Map.put(acc, x.position, x)
-
     end)
-
-    IO.inspect(newRedMap)
-    IO.inspect(newBlackMap)
 
     if(rem(pawn.position, 8) == 0) do
         pos0 = 100;
     end
-
     if(rem(pawn.position+1, 8) == 0) do
         pos1 = 100;
     end
-
-    if(pawn.player_color == "black") do
-        
+    if(pawn.player_color == "black") do   
         getBlackPlayer(newRedMap,newBlackMap,pos0,pos1,pawn)
-    
-    else
-        
+    else   
         getRedPlayer(newRedMap,newBlackMap,pos0,pos1,pawn)
     end
-
   end
 
+  #get next black position when opponent is red
   def getNextBlackMove(game,pawn) do
 
     dictmove = %{}
@@ -319,127 +261,88 @@ defmodule Checkers.Game do
     pawns = game[:pawns]
     pos0 = pawn.position - 9
     pos1 = pawn.position - 7
-    
     redPawns = pawns["red"]
     blackPawns = pawns["black"]
-
     newRedMap = %{}
     newBlackMap = %{}
 
     newRedMap = Enum.reduce(redPawns, %{},fn(x,acc) ->  
-
       Map.put(acc, x.position, x)
-
     end)
 
     newBlackMap = Enum.reduce(blackPawns, %{},fn(x,acc) ->  
-
       Map.put(acc, x.position, x)
-
     end)
 
     if(rem(pawn.position, 8) == 0) do
         pos0 = 100;
     end
-
     if(rem(pawn.position+1, 8) == 0) do
         pos1 = 100;
     end
-
-    if(pawn.player_color == "black") do
-        
-        getBlackPlayerPos(newRedMap,newBlackMap,pos0,pos1,pawn)
-    
-    else
-        
+    if(pawn.player_color == "black") do  
+        getBlackPlayerPos(newRedMap,newBlackMap,pos0,pos1,pawn) 
+    else       
         getRedPlayerPos(newRedMap,newBlackMap,pos0,pos1,pawn)
     end
-
   end
 
+  #get next red player pos when opponent is black
   def getRedPlayerPos(newRedMap,newBlackMap,pos0,pos1,pawn) do
 
       validPos = %{}
       cond do
-
         newBlackMap[pos0]!=nil ->
-
                pos0 = pawn.position - 18
                if((newRedMap[pos0]!=nil) or (newBlackMap[pos0]!=nil) or (rem(pos0 + 1, 8) == 0)) do
                 pos0 = 100
                end 
-
         newRedMap[pos0]!=nil ->
-
                 pos0 = 100
-
         true -> 
                 pos0
-
       end
-
       cond do
-
         newBlackMap[pos1]!=nil ->
-
                pos1 = pawn.position - 14
                if((newRedMap[pos1]!=nil) or (newBlackMap[pos1]!=nil) or (rem(pos1, 8) == 0)) do
                 pos1 = 100
                end 
-
         newRedMap[pos1]!=nil ->
-
                 pos1 = 100
-
         true ->
                 pos1
-
       end  
-
       validPos = %{pos0 => true, pos1 => true} 
   end
 
+  #get next black's pos when the oppenent is red
   def getBlackPlayerPos(newRedMap,newBlackMap,pos0,pos1,pawn) do
 
       validPos = %{}
       cond do
-
         newRedMap[pos0]!=nil ->
-
                pos0 = pawn.position - 18
                if((newRedMap[pos0]!=nil) or (newBlackMap[pos0]!=nil) or (rem(pos0 + 1, 8) == 0)) do
                 pos0 = 100
                end 
-
         newBlackMap[pos0]!=nil ->
-
                 pos0 = 100
-
         true ->
                 pos0
-
       end
-
       cond do
-
         newRedMap[pos1]!=nil ->
-
                pos1 = pawn.position - 14
                if((newRedMap[pos1]!=nil) or (newBlackMap[pos1]!=nil) or (rem(pos1, 8) == 0)) do
                 pos1 = 100
                end 
-
         newBlackMap[pos1]!=nil ->
-
                 pos1 = 100
-
         true -> 
                 pos1
-
       end  
-
       validPos = %{pos0 => true, pos1 => true}
-
   end
 
 end
